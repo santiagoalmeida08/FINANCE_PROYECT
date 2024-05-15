c7cc2b541fe18a535e1a9b1babf144247d436307
santiagoalmeida08 <santiago.almeida@udea.edu.co>
2024-05-15 17:04:46 -0500

    archivos_datos

---
+++ Analisis.py
@@ -0,0 +1,7 @@
+#Paquetes
+import pandas as pd
+
+
+#Importar datos 
+historic = pd.read_csv('https://raw.githubusercontent.com/juancamiloespana/LEA3_FIN/main/data/datos_historicos.csv')
+creditos = pd.read_csv('https://raw.githubusercontent.com/juancamiloespana/LEA3_FIN/main/data/datos_nuevos_creditos.csv')
