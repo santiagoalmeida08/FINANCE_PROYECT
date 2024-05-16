a6d45523affbaa195494535dcdb3443dc5fe5253
santiagoalmeida08 <santiago.almeida@udea.edu.co>
2024-05-16 09:51:34 -0500

    inicio de diseño

--- Analisis.py
+++ Analisis.py
@@ -5,3 +5,6 @@ import pandas as pd
 #Importar datos 
 historic = pd.read_csv('https://raw.githubusercontent.com/juancamiloespana/LEA3_FIN/main/data/datos_historicos.csv')
 creditos = pd.read_csv('https://raw.githubusercontent.com/juancamiloespana/LEA3_FIN/main/data/datos_nuevos_creditos.csv')
+
+historic.columns
+creditos.columns
